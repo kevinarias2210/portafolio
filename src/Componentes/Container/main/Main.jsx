d50b58885e682438aeb2e7d176dcadc4edaf8dce
Kevin Arias <kariasduarte@gmail.com>
2023-01-15 00:21:41 -0500

    main with responsive

--- src/Componentes/Container/main/Main.jsx
+++ src/Componentes/Container/main/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './main.scss';
 import photo from './img/yo.png';
+import circle from './img/circulo.png';
 
 export function Main(){
     return(
@@ -12,9 +13,11 @@ export function Main(){
                     <p className="main__container--p3">Desarrollador web full stack </p>
                 </div>
                 <div className="main__container--picture">
-                    <div className="main__container--circle"></div>
+                    <div className="main__container--circle">
+                        <img className="main__container--c" src={circle} alt="circle" />
+                    </div>
                     <div className="main__container--photo">
-                        <img src={photo} alt="My_photo" />
+                        <img className="main__container--img" src={photo} alt="My_photo" />
                     </div>
                 </div>
             </div>
